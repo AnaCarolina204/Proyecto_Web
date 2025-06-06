881d18168af2416cfe1046026c017c17c97cdb4b
AnaCarolina-04 <anacar9lina098@gmail.com>
2025-06-05 21:12:11 -0500

    seeders

--- Proyectoweb/database/seeders/DatabaseSeeder.php
+++ Proyectoweb/database/seeders/DatabaseSeeder.php
@@ -12,6 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            
+            _rolSeeder::class,
+            // _categorySeeder::class,
+            // _customerSeeder::class,
+            // _category_customerSeeder::class,
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
--- Proyectoweb/database/seeders/_rolSeeder.php
+++ Proyectoweb/database/seeders/_rolSeeder.php
@@ -2,16 +2,21 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class _rolSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('_rol')->insert([
+            ['rol_name' => 'Cliente'],
+            ['rol_name' => 'Operador'],
+            ['rol_name' => 'Marketing'],
+            ['rol_name' => 'Administrador'],
+        ]);
     }
 }
